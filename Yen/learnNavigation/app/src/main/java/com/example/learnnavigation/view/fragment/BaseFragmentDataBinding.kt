b870a4c631c838e87ec4166e9bd9055c1c73a2fc
Pham Vu Yen <99155789+PhamVuYen@users.noreply.github.com>
2023-12-25 11:41:02 +0700

    edit coding convetion

--- Yen/learnNavigation/app/src/main/java/com/example/learnnavigation/view/fragment/BaseFragmentDataBinding.kt
+++ Yen/learnNavigation/app/src/main/java/com/example/learnnavigation/view/fragment/BaseFragmentDataBinding.kt
@@ -1,28 +1,35 @@
 package com.example.learnnavigation.view.fragment
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseFragmentDataBinding<T : ViewBinding> : Fragment() {
+abstract class BaseFragmentDataBinding<T :ViewDataBinding> :
+    Fragment() {
 
-    lateinit var binding: T
-        private set
+    final  lateinit var binding: T
 
-    abstract val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> T
+    @get:LayoutRes
+    abstract val layoutId: Int
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = bindingInflater.invoke(inflater, container, false)
+        Log.d(TAG, "onCreateView: ()")
+        binding = DataBindingUtil.inflate<T>(inflater, layoutId, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
         return binding.root
     }
-
-    abstract val layoutId: Int
 }
 
--- Yen/learnNavigation/app/src/main/java/com/example/learnnavigation/view/fragment/FragmentBackgroundImage.kt
+++ Yen/learnNavigation/app/src/main/java/com/example/learnnavigation/view/fragment/FragmentBackgroundImage.kt
@@ -1,39 +1,25 @@
 package com.example.learnnavigation.view.fragment
 
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.fragment.app.viewModels
 import com.example.learnnavigation.R
 import com.example.learnnavigation.adapter.BackgroundImageAdapter
-import com.example.learnnavigation.databinding.FragmentAnimationImageBinding
 import com.example.learnnavigation.databinding.FragmentBackgroundImageBinding
-import com.example.learnnavigation.databinding.FragmentRingBinding
-import com.example.learnnavigation.model.SuggestedSearches
+
 import com.example.learnnavigation.viewmodel.BackgroundImageViewModel
-import kotlin.math.log
 
 
 class FragmentBackgroundImage : BaseFragmentDataBinding<FragmentBackgroundImageBinding>() {
 
-    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentBackgroundImageBinding =
-        FragmentBackgroundImageBinding::inflate
     override val layoutId: Int = R.layout.fragment_background_image
 
-    private lateinit var viewModel: BackgroundImageViewModel
+    private val viewModel: BackgroundImageViewModel by viewModels()
     private val adapter by lazy { BackgroundImageAdapter() }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        viewModel = ViewModelProvider(this).get(BackgroundImageViewModel::class.java)
-
         with(binding) {
             recycleViewChartFragment.adapter = adapter
 
