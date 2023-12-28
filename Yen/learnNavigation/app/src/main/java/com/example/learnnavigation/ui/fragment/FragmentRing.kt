329fedcf9d2188c872369e34d7e94fc166489bb2
Pham Vu Yen <99155789+PhamVuYen@users.noreply.github.com>
2023-12-28 16:32:07 +0700

    Create Dialog Yes/No Option

--- Yen/learnNavigation/app/src/main/java/com/example/learnnavigation/ui/fragment/FragmentRing.kt
+++ Yen/learnNavigation/app/src/main/java/com/example/learnnavigation/ui/fragment/FragmentRing.kt
@@ -1,18 +1,22 @@
 package com.example.learnnavigation.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.example.learnnavigation.R
 import com.example.learnnavigation.ui.adapter.AnimImageAdapter
 import com.example.learnnavigation.databinding.FragmentRingBinding
+import com.example.learnnavigation.ui.dialog.DialogYesNoOption
+import com.example.learnnavigation.ui.dialog.model.DialogData
 import com.example.learnnavigation.ui.viewmodel.RingViewModel
 import com.example.learnnavigation.utils.Const.HORIZONTAL_SPACE
 import com.example.learnnavigation.utils.Const.VERTICAL_SPACE
 import com.example.learnnavigation.utils.ItemSpacingDecoration
 class FragmentRing : BaseFragmentDataBinding<FragmentRingBinding,RingViewModel>() {
+//    private val diaogYesNo by lazy { DialogYesNoOption() }
     override val vm: RingViewModel   by viewModels()
-
     override val layoutId: Int = R.layout.fragment_ring
 
     private val adapter by lazy { AnimImageAdapter() }
@@ -32,6 +36,26 @@ class FragmentRing : BaseFragmentDataBinding<FragmentRingBinding,RingViewModel>(
             vm.fetchDataFromApi()
         }
 
+    }
+
+    override fun onInternetChange(isNetWork: Boolean) {
+        if(!isNetWork) {
+            val dialogData = DialogData(isLoading = false)
+            val dialog = DialogYesNoOption()
+            dialog.dialogData = dialogData
+            dialog.show(childFragmentManager, "YEN")
+            Log.d("Test","is network")
+        }
+        else{
+            val dialogData = DialogData(isLoading = true)
+            val dialog1 = DialogYesNoOption()
+            dialog1.dialogData = dialogData
+            dialog1.show(childFragmentManager, "YEN")
+            val dialog = childFragmentManager.findFragmentByTag("YEN") as? DialogYesNoOption
+            dialog?.dismiss()
+            Toast.makeText(context,"xdf",Toast.LENGTH_SHORT).show()
+            Log.d("Test","sffs")
+        }
     }
 }
 
